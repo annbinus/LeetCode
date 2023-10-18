c3511b9c3a30755a09a643bbe56dad136b23faef
annbinus <72045490+annbinus@users.noreply.github.com>
2023-10-17 23:06:16 -0400

    Time: 304 ms (5.00%) | Memory: 46 MB (5.28%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String reverseVowels(String s) {
+        Stack<Character> vowels = new Stack<Character>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' ||
+            s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U')
+            {
+                vowels.push(s.charAt(i));
+            }
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' ||
+            s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U')
+            {
+                s = s.substring(0, i) + vowels.pop() + s.substring(i+1);
+            }
+        }
+        return s;
+    }
+}
